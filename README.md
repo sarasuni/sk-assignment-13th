a0d2373990a4c623b3ca6e02ea4c5fda7e7738fb
Sara K <sarasuni2021@gmail.com>
2021-12-30 20:05:23 -0800

    E-commerce site with back end config

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# Project Title: Object-Relational Mapping (ORM): E-Commerce Back End
+
+
+## Table of Contents:
+- [Title](#title)
+- [Description](#description)
+- [Installation](#install)
+- [How to use](#usage) 
+- [Github Username](#username)
+- [Contact](#email)
+
+## Description:
+
+Building the back end for an e-commerce site by configuring a working Express.js API to use Sequelize to interact with a MySQL database.
+
+## Installation:
+
+npm install 
+
+## Usage:
+Demo Video Link:
+
+
+## Username:
+
+https://github.com/sarasuni
+
+## Email:
+
+Sarasuni2021@gmail.com
+
+
+
---
+++ src/models/Product.js
@@ -0,0 +1,59 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    // Define an id columns
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true
+    },
+    //Define product_name column
+    product_name: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    //Define price column
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull:false,
+      validate:{
+          isDecimal:true
+      }
+    },
+    //Define stock column
+    stock:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        defaultValue: 10,
+        validate:{
+          isNumeric:true
+        }
+      },
+    //define category_id column
+      category_id:{
+        type:DataTypes.INTEGER,
+        references:{
+          model:"category",
+          key:"id"
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
